90ccb56dae501247d01393ed88af42420eb7b317
makeevrserg <makeevrserg@gmail.com>
2023-04-22 18:53:35 +0300

    temporal config fix for multipaper server

--- spigot/src/main/kotlin/com/astrainteractive/astramarket/modules/Modules.kt
+++ spigot/src/main/kotlin/com/astrainteractive/astramarket/modules/Modules.kt
@@ -8,7 +8,10 @@ import com.astrainteractive.astramarket.plugin.AuctionConfig
 import com.astrainteractive.astramarket.plugin.Files
 import com.astrainteractive.astramarket.plugin.Translation
 import com.astrainteractive.astramarket.utils.toDBConnection
+import com.charleskorn.kaml.Yaml
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.bstats.bukkit.Metrics
 import ru.astrainteractive.astralibs.EmpireSerializer
 import ru.astrainteractive.astralibs.di.getValue
@@ -20,6 +23,7 @@ import ru.astrainteractive.astralibs.orm.DefaultDatabase
 import ru.astrainteractive.astralibs.utils.encoding.BukkitIOStreamProvider
 import ru.astrainteractive.astralibs.utils.encoding.Serializer
 import ru.astrainteractive.astralibs.utils.toClass
+import java.io.File
 
 object Modules {
     val bukkitSerializer = module {
@@ -29,7 +33,20 @@ object Modules {
         Translation()
     }
     val configuration = reloadable {
-        EmpireSerializer.toClass<AuctionConfig>(Files.configFile) ?: AuctionConfig()
+        val config = EmpireSerializer.toClass<AuctionConfig>(Files.configFile)
+
+        if (config == null){
+            AstraMarket.instance.dataFolder.mkdirs()
+            val oldConfig = File(Files.configFile.configFile.parentFile,"config.old.yml")
+            Files.configFile.configFile.copyTo(oldConfig).createNewFile()
+
+            Files.configFile.configFile.createNewFile()
+
+            Files.configFile.configFile.writeText(Yaml.default.encodeToString(AuctionConfig()))
+        }
+        val realConfig = config?:AuctionConfig()
+
+        return@reloadable realConfig
     }
     val database = module {
         val config by configuration
