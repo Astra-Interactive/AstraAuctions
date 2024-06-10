cf7b6eb81c23fb287ec8935362a8a4a757280425
makeevrserg <makeevrserg@gmail.com>
2024-06-10 19:46:36 +0300

    fix build

--- spigot/src/main/kotlin/ru/astrainteractive/astramarket/di/impl/BukkitCoreModuleImpl.kt
+++ spigot/src/main/kotlin/ru/astrainteractive/astramarket/di/impl/BukkitCoreModuleImpl.kt
@@ -6,8 +6,6 @@ import ru.astrainteractive.astralibs.encoding.encoder.ObjectEncoder
 import ru.astrainteractive.astralibs.event.EventListener
 import ru.astrainteractive.astralibs.kyori.KyoriComponentSerializer
 import ru.astrainteractive.astralibs.lifecycle.Lifecycle
-import ru.astrainteractive.astralibs.logging.JUtilFileLogger
-import ru.astrainteractive.astralibs.logging.Logger
 import ru.astrainteractive.astralibs.menu.event.DefaultInventoryClickEvent
 import ru.astrainteractive.astramarket.AstraMarket
 import ru.astrainteractive.astramarket.di.BukkitCoreModule
@@ -16,7 +14,6 @@ import ru.astrainteractive.klibs.kdi.Lateinit
 import ru.astrainteractive.klibs.kdi.Reloadable
 import ru.astrainteractive.klibs.kdi.Single
 import ru.astrainteractive.klibs.kdi.getValue
-import java.io.File
 
 class BukkitCoreModuleImpl : BukkitCoreModule {
 
@@ -32,14 +29,6 @@ class BukkitCoreModuleImpl : BukkitCoreModule {
 
     override val kyoriComponentSerializer: Reloadable<KyoriComponentSerializer> = Reloadable {
         KyoriComponentSerializer.Legacy
-    }
-
-    override val logger: Single<Logger> = Single {
-        JUtilFileLogger(
-            tag = "AstraMarket",
-            folder = File(plugin.value.dataFolder, "logs"),
-            logger = plugin.value.logger
-        )
     }
 
     private val bStatsFactory = Factory {
