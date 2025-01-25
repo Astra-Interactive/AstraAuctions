d151532d267c3d3c59eb7491f0ad077558c8c196
Roman Makeev <makeevrserg@gmail.com>
2025-01-25 11:25:02 +0300

    fix deps

--- modules/command-bukkit/src/main/kotlin/ru/astrainteractive/astramarket/command/auction/AuctionCommandErrorHandler.kt
+++ modules/command-bukkit/src/main/kotlin/ru/astrainteractive/astramarket/command/auction/AuctionCommandErrorHandler.kt
@@ -3,18 +3,19 @@ package ru.astrainteractive.astramarket.command.auction
 import ru.astrainteractive.astralibs.command.api.context.BukkitCommandContext
 import ru.astrainteractive.astralibs.command.api.error.ErrorHandler
 import ru.astrainteractive.astralibs.command.api.exception.DefaultCommandException
+import ru.astrainteractive.astralibs.command.api.exception.NoPermissionException
 import ru.astrainteractive.astramarket.command.auction.di.AuctionCommandDependencies
 
 internal class AuctionCommandErrorHandler(
     private val dependencies: AuctionCommandDependencies
 ) : ErrorHandler<BukkitCommandContext>,
     AuctionCommandDependencies by dependencies {
-    override fun handle(commandContext: BukkitCommandContext, throwable: Throwable) {
+    override fun handle(ctx: BukkitCommandContext, throwable: Throwable) {
         when (throwable) {
             is DefaultCommandException -> with(kyoriComponentSerializer) {
                 when (throwable) {
-                    is DefaultCommandException.NoPermissionException -> {
-                        commandContext.sender.sendMessage(translation.general.noPermissions.component)
+                    is NoPermissionException -> {
+                        ctx.sender.sendMessage(translation.general.noPermissions.component)
                     }
 
                     else -> error("BadArgumentException not handled")
@@ -22,15 +23,15 @@ internal class AuctionCommandErrorHandler(
             }
 
             AuctionCommand.Error.NotPlayer -> with(kyoriComponentSerializer) {
-                commandContext.sender.sendMessage(translation.general.onlyForPlayers.component)
+                ctx.sender.sendMessage(translation.general.onlyForPlayers.component)
             }
 
             AuctionCommand.Error.WrongPrice -> with(kyoriComponentSerializer) {
-                commandContext.sender.sendMessage(translation.auction.wrongPrice.component)
+                ctx.sender.sendMessage(translation.auction.wrongPrice.component)
             }
 
             AuctionCommand.Error.WrongUsage -> with(kyoriComponentSerializer) {
-                commandContext.sender.sendMessage(translation.general.wrongArgs.component)
+                ctx.sender.sendMessage(translation.general.wrongArgs.component)
             }
 
             else -> error("${throwable::class} not handled")
--- modules/command-bukkit/src/main/kotlin/ru/astrainteractive/astramarket/command/auction/AuctionCommandRegistry.kt
+++ modules/command-bukkit/src/main/kotlin/ru/astrainteractive/astramarket/command/auction/AuctionCommandRegistry.kt
@@ -1,13 +1,13 @@
 package ru.astrainteractive.astramarket.command.auction
 
-import ru.astrainteractive.astralibs.command.api.util.PluginExt.registerCommand
+import ru.astrainteractive.astralibs.command.api.util.PluginExt.setCommandExecutor
 import ru.astrainteractive.astramarket.command.auction.di.AuctionCommandDependencies
 
 internal class AuctionCommandRegistry(
     private val dependencies: AuctionCommandDependencies
 ) {
     fun register() {
-        dependencies.plugin.registerCommand(
+        dependencies.plugin.setCommandExecutor(
             alias = "amarket",
             commandParser = AuctionCommandParser(),
             commandExecutor = AuctionCommandExecutor(dependencies),
