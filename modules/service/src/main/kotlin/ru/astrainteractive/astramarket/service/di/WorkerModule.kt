b95f31ec733fd13cf2d390bc0cdba507c20b402d
Roman Makeev <makeevrserg@gmail.com>
2025-10-29 02:19:49 +0300

    remove lazy from service

--- modules/service/src/main/kotlin/ru/astrainteractive/astramarket/service/di/WorkerModule.kt
+++ modules/service/src/main/kotlin/ru/astrainteractive/astramarket/service/di/WorkerModule.kt
@@ -2,7 +2,6 @@ package ru.astrainteractive.astramarket.service.di
 
 import kotlinx.coroutines.flow.flowOf
 import ru.astrainteractive.astralibs.lifecycle.Lifecycle
-import ru.astrainteractive.astralibs.service.Service
 import ru.astrainteractive.astralibs.service.TickFlowService
 import ru.astrainteractive.astramarket.core.di.CoreModule
 import ru.astrainteractive.astramarket.di.ApiMarketModule
@@ -16,22 +15,20 @@ interface WorkerModule {
         apiMarketModule: ApiMarketModule,
         coreModule: CoreModule
     ) : WorkerModule {
-        private val expireServiceExecutor: Service by lazy {
-            TickFlowService(
-                coroutineContext = coreModule.dispatchers.IO,
-                delay = flowOf(1.minutes),
-                executor = ExpireServiceExecutor(
-                    marketApi = apiMarketModule.marketApi,
-                    configKrate = coreModule.configKrate
-                )
+        private val expireService = TickFlowService(
+            coroutineContext = coreModule.dispatchers.IO,
+            delay = flowOf(1.minutes),
+            executor = ExpireServiceExecutor(
+                marketApi = apiMarketModule.marketApi,
+                configKrate = coreModule.configKrate
             )
-        }
+        )
         override val lifecycle: Lifecycle = Lifecycle.Lambda(
             onEnable = {
-                expireServiceExecutor.onCreate()
+                expireService.onCreate()
             },
             onDisable = {
-                expireServiceExecutor.onDestroy()
+                expireService.onDestroy()
             }
         )
     }
